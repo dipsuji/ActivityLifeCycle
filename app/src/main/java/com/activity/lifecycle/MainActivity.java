21f216beefc4f0a3c52cf92993ab9c0714efa9ca
Sujata Maurya <mauryasujata69@gmail.com>
2018-09-22 13:55:46 +0900

    Added second activity logs
    Added second activity logs

--- app/src/main/java/com/activity/lifecycle/MainActivity.java
+++ app/src/main/java/com/activity/lifecycle/MainActivity.java
@@ -16,14 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.d(TAG, "onCreate....................");
-        Toast.makeText(this,"onCreate..........", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "First Activity onCreate....................");
+        Toast.makeText(this,"First Activity onCreate..........", Toast.LENGTH_SHORT).show();
 
         nextButton = findViewById(R.id.next_button);
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,"onStart..........", Toast.LENGTH_SHORT).show();
                 // start new activity
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                 startActivity(intent);
@@ -34,43 +33,43 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart....................");
-        Toast.makeText(this,"onStart..........", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "First Activity onStart....................");
+        Toast.makeText(this,"First Activity onStart..........", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume....................");
-        Toast.makeText(this,"onResume..........", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "First Activity onResume....................");
+        Toast.makeText(this,"First Activity onResume..........", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause....................");
-        Toast.makeText(this,"onPause..........", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "First Activity onPause....................");
+        Toast.makeText(this,"First Activity onPause..........", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop....................");
-        Toast.makeText(this,"onStop..........", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "First Activity onStop....................");
+        Toast.makeText(this,"First Activity onStop..........", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy....................");
-        Toast.makeText(this,"onDestroy..........", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "First Activity onDestroy....................");
+        Toast.makeText(this,"First Activity onDestroy..........", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG, "onRestart....................");
-        Toast.makeText(this,"onRestart..........", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "First Activity onRestart....................");
+        Toast.makeText(this,"First Activity onRestart..........", Toast.LENGTH_SHORT).show();
     }
 
 }
--- app/src/main/java/com/activity/lifecycle/SecondActivity.java
+++ app/src/main/java/com/activity/lifecycle/SecondActivity.java
@@ -3,14 +3,20 @@ package com.activity.lifecycle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
+    private static final String TAG = SecondActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Log.d(TAG, "Second Activity onCreate....................");
+        Toast.makeText(this,"Second Activity onCreate..........", Toast.LENGTH_SHORT).show();
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
@@ -26,5 +32,48 @@ public class SecondActivity extends AppCompatActivity {
                 finish();
         }
         return true;
+
     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "Second Activity onStart....................");
+        Toast.makeText(this,"Second Activity onStart..........", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Second Activity onResume....................");
+        Toast.makeText(this,"Second Activity onResume..........", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Second Activity onPause....................");
+        Toast.makeText(this,"Second Activity onPause..........", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "Second Activity onStop....................");
+        Toast.makeText(this,"Second Activity onStop..........", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Second Activity onDestroy....................");
+        Toast.makeText(this,"Second Activity onDestroy..........", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "Second Activity onRestart....................");
+        Toast.makeText(this,"Second Activity onRestart..........", Toast.LENGTH_SHORT).show();
+    }
+
 }
